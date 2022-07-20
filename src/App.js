2b13f3605ca9f4282914210342b41f2a60b52dee
Emanuel Palacio <emanuel.palacio.arq@gmail.com>
2022-07-20 18:49:45 -0300

    agrega nuevo componente - ItemListContainer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import ItemListContainer from './components/ItemListContainer';
 import NavBar from './components/NavBar'
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <ItemListContainer greeting='Hola mundo'/>
     </div>
   );
 }
