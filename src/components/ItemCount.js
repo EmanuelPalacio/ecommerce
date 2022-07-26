d36ee4d8030a05644de1cf5e546097dbb54a1ae5
Emanuel Palacio <emanuel.palacio.arq@gmail.com>
2022-07-26 16:45:14 -0300

    Optimización de  codigo del componente ItemCount

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -3,14 +3,14 @@ import Button from "./Button";
 import "./styles/card.css"
 
 export default function ItemCount(props) {
-    const [contador, setContador] = useState(0);
+    const [contador, setContador] = useState(parseInt(props.initial));
     const sumar = () => contador < parseInt(props.stock) && setContador(contador + 1);
-    const restar = () => contador > 0 && setContador(contador - 1);
+    const restar = () => contador > parseInt(props.initial) && setContador(contador - 1);
 
     return (
     <div className="card">
         <h2>Hooks - useState - contador</h2>
-        <span>{isNaN(props.initial) ? contador : contador + parseInt(props.initial) }</span>
+        <span>{contador}</span>
         <Button content="-" style="btn" event={restar}/>
         <Button content="+" style="btn" event={sumar}/>
     </div>
