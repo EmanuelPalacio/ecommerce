f3b429c680f7bfee3d8e710db00bda5173d2dc0f
Emanuel Palacio <emanuel.palacio.arq@gmail.com>
2022-08-02 17:37:58 -0300

    install react roter dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import ItemDetailContainer from './components/ItemDetailContainer';
 import ItemListContainer from './components/ItemListContainer';
 import NavBar from './components/NavBar';
+import "./components/styles/style.css";
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
-      <ItemListContainer greeting='Hola mundo'/>
+      {/* <ItemListContainer greeting='Hola mundo'/> */}
       <ItemDetailContainer/>
     </div>
   );
--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,14 +1,15 @@
 import ItemCount from "./ItemCount";
+import "./styles/itemDetail.css"
 
 export default function ItemDetail({item}){
     return (
-        <div className="flex bg-danger">
-            <h3>ItemDetail</h3>
-            <h3>{item.title}</h3>
-            <p>{item.description}</p>
-            <img src={`${item.imagen}`}></img>
-
-            <ItemCount stock={5} initial={1} />
-        </div>
+        <main className="itemDetail">
+            <div className="itemDetail__container">
+                <h3 className="itemDetail__title">{item.title}</h3>
+                <p className="itemDetail__description">{item.description}Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione, reprehenderit? Maxime explicabo hic ducimus dignissimos cumque officiis eaque animi delectus, suscipit commodi aut doloribus! Quae non commodi ea deleniti natus.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione, reprehenderit? Maxime explicabo hic ducimus dignissimos cumque officiis eaque animi delectus, suscipit commodi aut doloribus! Quae non commodi ea deleniti natus.</p>
+                <img className="itemDetail__img" src={`${item.imagen}`}/>
+                <ItemCount className="itemDetail__count" stock={5} initial={1} />
+            </div>
+        </main>
     )
 }
