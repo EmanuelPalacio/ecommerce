7baca96b3b02dbb59101ee81292e30bbcabd30ef
Emanuel Palacio <emanuel.palacio.arq@gmail.com>
2022-09-05 19:45:36 -0300

    finish

--- src/App.js
+++ src/App.js
@@ -18,10 +18,10 @@ function App() {
             <NavBar/>
             <Routes>
               <Route exact path='/' element={<ItemListContainer />} />
-              <Route path="/item/:id" element={<ItemDetailContainer />} />
-              <Route path="/category/:category" element={<ItemListContainer />}/>
-              <Route path="/cart" element={<CartContainer/>} />
-              <Route path="*" element={<Error404 text="Ups, no encontramos lo que esta buscando"/>} />
+              <Route exact path="/item/:id" element={<ItemDetailContainer />} />
+              <Route exact path="/category/:category" element={<ItemListContainer />}/>
+              <Route exact path="/cart" element={<CartContainer/>} />
+              <Route exact path="*" element={<Error404 text="Ups, no encontramos lo que esta buscando"/>} />
             </Routes>
           </BrowserRouter>
         </CartProvider>
